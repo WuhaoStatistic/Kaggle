550b4ce556ba33f435e0a5e2f268da661abe349c
WuhaoStatistic <89610539+WuhaoStatistic@users.noreply.github.com>
2022-04-16 18:07:34 +0200

    Add files via upload

---
+++ Happywhale - Whale and Dolphin Identification/myutils.py
@@ -0,0 +1,148 @@
+import torch
+from torch.utils.data import DataLoader
+from prefetch_generator import BackgroundGenerator
+import torch.nn as nn
+import math
+import torch.nn.functional as F
+
+
+# 据说能加快读取速度
+class PrefetchDataLoader(DataLoader):
+    '''
+        replace DataLoader with PrefetchDataLoader, it can accelerate data loading
+    '''
+
+    def __iter__(self):
+        return BackgroundGenerator(super().__iter__())
+
+
+# SPP池化层
+class SPPLayer(nn.Module):
+
+    def __init__(self, num_levels, pool_type='max_pool'):
+        super(SPPLayer, self).__init__()
+
+        self.num_levels = num_levels
+        self.pool_type = pool_type
+
+    def forward(self, x):
+        bs, c, h, w = x.size()
+        pooling_layers = []
+        for i in range(self.num_levels):
+            kernel_size = h // (2 ** i)
+            if self.pool_type == 'max_pool':
+                tensor = F.max_pool2d(x, kernel_size=kernel_size,
+                                      stride=kernel_size).view(bs, -1)
+            else:
+                tensor = F.avg_pool2d(x, kernel_size=kernel_size,
+                                      stride=kernel_size).view(bs, -1)
+            pooling_layers.append(tensor)
+        x = torch.cat(pooling_layers, dim=-1)
+        return x
+
+
+# ArcFace
+class ArcMarginProduct(nn.Module):
+    def __init__(self, in_features, out_features, device='cuda', s=30.0,
+                 m=0.50, easy_margin=False, ls_eps=0.0):
+        '''
+        in_features: dimension of the input
+        out_features: dimension of the last layer (in our case the classification)
+        s: norm of input feature
+        m: margin
+        ls_eps: label smoothing
+        '''
+        super(ArcMarginProduct, self).__init__()
+        self.in_features, self.out_features = in_features, out_features
+        self.s = s
+        self.m = m
+        self.device = device
+        self.ls_eps = ls_eps
+        self.weight = nn.Parameter(torch.FloatTensor(out_features, in_features))
+        # Fills the input `Tensor` with values according to the method described in
+        # `Understanding the difficulty of training deep feedforward neural networks`
+        # Glorot, X. & Bengio, Y. (2010)
+        # using a uniform distribution.
+        nn.init.xavier_uniform_(self.weight)
+
+        self.easy_margin = easy_margin
+        self.cos_m, self.sin_m = math.cos(m), math.sin(m)
+        self.th = math.cos(math.pi - m)
+        self.mm = math.sin(math.pi - m) * m
+
+    def forward(self, input, label):
+        # --------------------------- cos(theta) & phi(theta) ---------------------
+        cosine = F.linear(F.normalize(input), F.normalize(self.weight))
+        sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
+        phi = cosine * self.cos_m - sine * self.sin_m
+        if self.easy_margin:
+            phi = torch.where(cosine > 0, phi, cosine)
+        else:
+            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
+        # --------------------------- convert label to one-hot ---------------------
+        one_hot = torch.zeros(cosine.size()).to(self.device)
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+        if self.ls_eps > 0:
+            one_hot = (1 - self.ls_eps) * one_hot + self.ls_eps / self.out_features
+        # -------------torch.where(out_i = {x_i if condition_i else y_i) ------------
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        output *= self.s
+
+        return output
+
+
+class GeM(nn.Module):
+    def __init__(self, p=3, eps=1e-6):
+        super(GeM, self).__init__()
+        self.p = nn.Parameter(torch.ones(1) * p)
+        self.eps = eps
+
+    def forward(self, x):
+        return self.gem(x, p=self.p, eps=self.eps)
+
+    def gem(self, x, p=3, eps=1e-6):
+        return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1. / p)
+
+    def __repr__(self):
+        return self.__class__.__name__ + \
+               '(' + 'p=' + '{:.4f}'.format(self.p.data.tolist()[0]) + \
+               ', ' + 'eps=' + str(self.eps) + ')'
+
+
+# 通道注意力机制
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc1 = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+
+# 空间注意力机制
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
